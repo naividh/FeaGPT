442d538e2b17c7a79015bb078f8d6b4c5707a476
naividh <155294476+naividh@users.noreply.github.com>
2026-03-02 13:33:14 +0530

    fix: remove broken imports from utils/__init__.py

--- feagpt/utils/__init__.py
+++ feagpt/utils/__init__.py
@@ -1,4 +1,5 @@
 """FeaGPT utilities package."""
-from feagpt.utils.units import UnitConverter
 from feagpt.utils.file_io import WorkspaceManager
-from feagpt.utils.resource_monitor import ResourceMonitor
+
+# Units module uses standalone functions:
+# from feagpt.utils.units import convert_length, convert_stress
